8d88d1410745b7d546360225bfd9b01c2533474f
mo100saad <xrblazettv@gmail.com>
2025-11-08 17:25:34 -0500

    Variables declared

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ from dotenv import load_dotenv #to load in env variables
 
 # Load environment variables
 load_dotenv() #user names pass etc.
+
+# declare expected hidden variables to connect to server
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+#Use the vars to connect to postgress
+def get_connection():
+    return psycopg2.connect( 
+        host=DB_HOST,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
