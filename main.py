11f56ad2e194491fd7ed5c1f920a5eba061448c7
mo100saad <xrblazettv@gmail.com>
2025-11-08 17:24:04 -0500

    Imports added

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ main.py
 COMP3005 Assignment Q1
 Performs CRUD operations on the 'students' table in PostgreSQL. 
 """
+import os #for retrieving env variables
+import psycopg2 #communicating with postgress server
+from dotenv import load_dotenv #to load in env variables
+
+
+# Load environment variables
+load_dotenv() #user names pass etc.
